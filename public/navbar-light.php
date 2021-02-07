73d2e8435c7f86f85e29e6bb3a065a75ef005c85
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-02-07 15:55:02 +0100

    hamburger nav icon

--- public/navbar-light.php
+++ public/navbar-light.php
@@ -20,7 +20,8 @@
             <a href="index.php"><img src="img/logo.png" class="w-5/12" alt="Logo"></a>
         </div>
     
-        <label for="menu-toggle" class="cursor-pointer block"><img src="img/back-arrow.png" alt="">
+        <label for="menu-toggle" class="cursor-pointer block">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
     </div>
 
